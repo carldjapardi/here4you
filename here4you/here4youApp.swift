311927db7490739ea11e7fcddabd17bce97cd425
carldjapardi <carldjapardi7@gmail.com>
2025-11-20 22:32:08 -0800

    initialized swift

---
+++ here4you/here4youApp.swift
@@ -0,0 +1,17 @@
+//
+//  here4youApp.swift
+//  here4you
+//
+//  Created by Carl on 11/20/25.
+//
+
+import SwiftUI
+
+@main
+struct here4youApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ here4youTests/here4youTests.swift
@@ -0,0 +1,17 @@
+//
+//  here4youTests.swift
+//  here4youTests
+//
+//  Created by Carl on 11/20/25.
+//
+
+import Testing
+@testable import here4you
+
+struct here4youTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
